46a6b91d289f61c440826bde3508ee575d2088ab
apenny23 <apenny23@kentdenver.org>
2022-02-24 23:11:24 -0700

    Created file and default constructor

---
+++ src/ArrayDictionary.java
@@ -0,0 +1,10 @@
+public class ArrayDictionary <T,E> {
+    private ArrayList<E> tags;
+    private ArrayList<T> Vals;
+
+    public ArrayDictionary() {
+        tags = new ArrayList<>();
+        Vals = new ArrayList<>();
+    }
+
+}
